6563ab81e3aa665e28abfa57065992915cb60cbf
Tai Nguyen <ngn.tai@gmail.com>
2022-05-19 16:56:46 +0200

    mon gestionnaire de movies

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,39 @@
+class ListsController < ApplicationController
+
+  before_action :set_list, only: [:show]
+  def index
+    @lists = List.all
+  end
+
+  def show
+    bookmarks = Bookmark.where("list_id = #{params[:id]}")
+    @hash_array = []
+    bookmarks.each do |bookmark|
+      @hash_array << {movie: Movie.find(bookmark[:movie_id]), comment: bookmark.comment, bookmark_id: bookmark.id}
+    end
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "json"
+require "open-uri"
+
+
+puts "Cleaning database..."
+Movie.destroy_all
+
+5.times do
+
+
+  url = "https://tmdb.lewagon.com/movie/top_rated?api_key=%3Cyour_api_key%3E"
+  movie_serialized = URI.open(url).read
+  movie_temp = JSON.parse(movie_serialized)
+  movie = movie_temp["results"].sample
+
+  new_movie = Movie.new(
+    title:  movie["original_title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}",
+    rating: rand(0.1..10.0)
+  )
+  new_movie.save!
+end
+puts 'Finished!'
